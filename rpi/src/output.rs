1f1051110004de9259ec10ff9d36cfae1ae9590b
Edward Källstedt <edward.kallstedt@gmail.com>
2024-06-28 18:17:34 +0200

    cargofmt

--- rpi/src/output.rs
+++ rpi/src/output.rs
@@ -13,7 +13,7 @@ impl Output for DebugOutput {
     fn render(&mut self, render_board: &RenderBoard) -> Result<()> {
         for row in 0..12 {
             for col in 0..12 {
-                let color = render_board.get(col, 11-row);
+                let color = render_board.get(col, 11 - row);
                 print!("({},{},{}) ", color.r, color.g, color.b);
             }
             println!();
