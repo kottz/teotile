add4044d042384fe6c95bf856abcff1a25ba12dc
Edward Källstedt <edward.kallstedt@gmail.com>
2024-07-02 13:03:50 +0200

    maze: make win animation to player color

--- engine/src/game/maze.rs
+++ engine/src/game/maze.rs
@@ -1,7 +1,7 @@
 use crate::{
     animation::Animation,
     game::{CommandType, Game, GameCommand, RenderBoard, RGB},
-    GameError, Player,
+    GameError, Player as GamePlayer,
 };
 
 use crate::random::CustomRng;
@@ -13,14 +13,26 @@ const VICTORY_ANIMATION_DURATION: Duration = Duration::from_secs(5);
 const VICTORY_ANIMATION_SPEED: Duration = Duration::from_millis(100);
 const GRID_SIZE: usize = 12;
 
+struct Player {
+    position: (usize, usize),
+    color: RGB,
+}
+
+impl Player {
+    fn new(position: (usize, usize), color: RGB) -> Self {
+        Self { position, color }
+    }
+}
+
 pub struct MazeGame {
     board: MazeBoard,
     state: MazeGameState,
     mode: MazeGameMode,
-    player_pos: [(usize, usize); 2],
+    players: SmallVec<[Player; 2]>,
     exit_pos: (usize, usize),
     victory_animation: Animation,
     current_time: Duration,
+    winning_player: Option<usize>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -29,6 +41,7 @@ enum MazeGameState {
     Victory(Duration),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum MazeGameMode {
     Normal,
     Multiplayer,
@@ -127,26 +140,63 @@ impl MazeBoard {
 impl MazeGame {
     pub fn new(seed: u64, mode: MazeGameMode) -> Self {
         let board = MazeBoard::new(seed);
-        let player_pos = [(1, 1); 2];
-        let exit_pos = board.find_furthest_tile(player_pos[0]);
+        let start_pos = (1, 1);
+        let exit_pos = board.find_furthest_tile(start_pos);
+
+        let mut players = SmallVec::new();
+        players.push(Player::new(start_pos, RGB::new(0, 255, 0))); // Player 1: Green
+
+        if let MazeGameMode::Multiplayer | MazeGameMode::FlashLightMultiplayer = mode {
+            players.push(Player::new((1, 2), RGB::new(0, 0, 255))); // Player 2: Blue
+        }
+
         Self {
             board,
             state: MazeGameState::Playing,
             mode,
-            player_pos,
+            players,
             exit_pos,
             victory_animation: Animation::new(VICTORY_ANIMATION_SPEED),
             current_time: Duration::from_millis(0),
+            winning_player: None,
         }
     }
 
-    fn generate_psychedelic_color(&self, row: usize, col: usize, time: Duration) -> RGB {
+    fn generate_psychedelic_color(
+        &self,
+        row: usize,
+        col: usize,
+        time: Duration,
+        winner_color: RGB,
+    ) -> RGB {
         let time_factor = time.as_secs_f32() * 2.0;
-        let r = ((libm::sinf((row as f32 * 0.3 + time_factor) * 2.0) + 1.0) * 127.5) as u8;
-        let g = ((libm::sinf((col as f32 * 0.3 + time_factor) * 2.0 + 2.094) + 1.0) * 127.5) as u8;
-        let b = ((libm::sinf((row as f32 + col as f32) * 0.3 + time_factor * 2.0 + 4.188) + 1.0)
+        let base_r = ((libm::sinf((row as f32 * 0.3 + time_factor) * 2.0) + 1.0) * 127.5) as u8;
+        let base_g =
+            ((libm::sinf((col as f32 * 0.3 + time_factor) * 2.0 + 2.094) + 1.0) * 127.5) as u8;
+        let base_b = ((libm::sinf((row as f32 + col as f32) * 0.3 + time_factor * 2.0 + 4.188)
+            + 1.0)
             * 127.5) as u8;
+
+        // Blend the base psychedelic color with the winner's color
+        let blend_factor = 0.7; // Adjust this to control how prominent the winner's color is
+        let r = (base_r as f32 * (1.0 - blend_factor) + winner_color.r as f32 * blend_factor) as u8;
+        let g = (base_g as f32 * (1.0 - blend_factor) + winner_color.g as f32 * blend_factor) as u8;
+        let b = (base_b as f32 * (1.0 - blend_factor) + winner_color.b as f32 * blend_factor) as u8;
+
         RGB::new(r, g, b)
+    }
+    fn reset_game(&mut self) {
+        self.board = MazeBoard::new(self.board.seed + 1);
+        self.state = MazeGameState::Playing;
+        self.winning_player = None;
+
+        let start_pos = (1, 1);
+        self.exit_pos = self.board.find_furthest_tile(start_pos);
+
+        self.players[0].position = start_pos;
+        if self.players.len() > 1 {
+            self.players[1].position = (1, 2);
+        }
     }
 }
 
@@ -162,51 +212,43 @@ impl Game for MazeGame {
                     _ => (0, 0),
                 };
 
-                let player_index = match self.mode {
-                    MazeGameMode::Normal | MazeGameMode::FlashLight => 0,
-                    MazeGameMode::Multiplayer | MazeGameMode::FlashLightMultiplayer => {
-                        match input.player {
-                            Player::Player1 => 0,
-                            Player::Player2 => 1,
-                        }
-                    }
+                let player_index = match (self.mode, input.player) {
+                    (MazeGameMode::Normal | MazeGameMode::FlashLight, _) => 0,
+                    (_, GamePlayer::Player1) => 0,
+                    (_, GamePlayer::Player2) => 1,
                 };
 
-                let (x, y) = self.player_pos[player_index];
-                let nx = x as isize + dx;
-                let ny = y as isize + dy;
+                if let Some(player) = self.players.get_mut(player_index) {
+                    let (x, y) = player.position;
+                    let nx = x as isize + dx;
+                    let ny = y as isize + dy;
 
-                if nx >= 0
-                    && nx < GRID_SIZE as isize
-                    && ny >= 0
-                    && ny < GRID_SIZE as isize
-                    && self.board.tiles[nx as usize][ny as usize] != MazeTile::Wall
-                {
-                    self.player_pos[player_index] = (nx as usize, ny as usize);
-                }
+                    if nx >= 0
+                        && nx < GRID_SIZE as isize
+                        && ny >= 0
+                        && ny < GRID_SIZE as isize
+                        && self.board.tiles[nx as usize][ny as usize] != MazeTile::Wall
+                    {
+                        player.position = (nx as usize, ny as usize);
+                    }
 
-                if self.player_pos[player_index] == self.exit_pos {
-                    self.state = MazeGameState::Victory(Duration::ZERO);
+                    if player.position == self.exit_pos {
+                        self.state = MazeGameState::Victory(Duration::ZERO);
+                        self.winning_player = Some(player_index);
+                    }
                 }
             }
             MazeGameState::Victory(elapsed_time) => {
                 if input.command_type == CommandType::Select
                     && *elapsed_time >= VICTORY_ANIMATION_DURATION
                 {
-                    self.board = MazeBoard::new(self.board.seed + 1);
-                    self.player_pos = match self.mode {
-                        MazeGameMode::Normal | MazeGameMode::FlashLight => [(1, 1), (0, 0)],
-                        MazeGameMode::Multiplayer | MazeGameMode::FlashLightMultiplayer => {
-                            [(1, 1), (1, 2)]
-                        }
-                    };
-                    self.exit_pos = self.board.find_furthest_tile(self.player_pos[0]);
-                    self.state = MazeGameState::Playing;
+                    self.reset_game();
                 }
             }
         }
         Ok(())
     }
+
     fn update(&mut self, delta_time: Duration) -> Result<(), GameError> {
         self.current_time += delta_time;
 
@@ -216,15 +258,7 @@ impl Game for MazeGame {
                 *elapsed_time += delta_time;
                 self.victory_animation.update(self.current_time);
                 if *elapsed_time >= VICTORY_ANIMATION_DURATION {
-                    self.state = MazeGameState::Playing;
-                    self.board = MazeBoard::new(self.board.seed + 1);
-                    self.player_pos = match self.mode {
-                        MazeGameMode::Normal | MazeGameMode::FlashLight => [(1, 1), (0, 0)],
-                        MazeGameMode::Multiplayer | MazeGameMode::FlashLightMultiplayer => {
-                            [(1, 1), (1, 2)]
-                        }
-                    };
-                    self.exit_pos = self.board.find_furthest_tile(self.player_pos[0]);
+                    self.reset_game();
                 }
             }
         }
@@ -234,144 +268,91 @@ impl Game for MazeGame {
     fn render(&self) -> Result<RenderBoard, GameError> {
         let mut render_board = RenderBoard::new();
         match &self.state {
-            MazeGameState::Playing => match &self.mode {
-                MazeGameMode::Normal | MazeGameMode::Multiplayer => {
-                    for x in 0..GRID_SIZE {
-                        for y in 0..GRID_SIZE {
-                            let rgb = match self.board.tiles[x][y] {
-                                MazeTile::Empty => RGB::new(0, 0, 0),
-                                MazeTile::Wall => RGB::new(255, 255, 255),
-                            };
-                            render_board.set(x, y, rgb);
+            MazeGameState::Playing => {
+                match &self.mode {
+                    MazeGameMode::Normal | MazeGameMode::Multiplayer => {
+                        // Render maze
+                        for x in 0..GRID_SIZE {
+                            for y in 0..GRID_SIZE {
+                                let rgb = match self.board.tiles[x][y] {
+                                    MazeTile::Empty => RGB::new(0, 0, 0),
+                                    MazeTile::Wall => RGB::new(255, 255, 255),
+                                };
+                                render_board.set(x, y, rgb);
+                            }
+                        }
+                        // Render exit
+                        render_board.set(self.exit_pos.0, self.exit_pos.1, RGB::new(255, 0, 0));
+                        // Render players
+                        for player in &self.players {
+                            render_board.set(player.position.0, player.position.1, player.color);
                         }
                     }
-                    render_board.set(self.exit_pos.0, self.exit_pos.1, RGB::new(255, 0, 0));
-                    render_board.set(
-                        self.player_pos[0].0,
-                        self.player_pos[0].1,
-                        RGB::new(0, 255, 0),
-                    );
-                    if let MazeGameMode::Multiplayer = self.mode {
-                        render_board.set(
-                            self.player_pos[1].0,
-                            self.player_pos[1].1,
-                            RGB::new(0, 0, 255),
-                        );
-                    }
-                }
-                MazeGameMode::FlashLight => {
-                    let distance = |x: usize, y: usize| {
-                        let dx = x as isize - self.player_pos[0].0 as isize;
-                        let dy = y as isize - self.player_pos[0].1 as isize;
-                        sqrt((dx * dx + dy * dy) as f64)
-                    };
-                    for x in 0..GRID_SIZE {
-                        for y in 0..GRID_SIZE {
-                            let d = distance(x, y);
-                            let wall_intensity = (255.0 * (1.0 - d / 3.0)) as u8;
-                            let wall_color =
-                                RGB::new(wall_intensity, wall_intensity, wall_intensity);
-                            let rgb = match self.board.tiles[x][y] {
-                                MazeTile::Empty => RGB::new(0, 0, 0),
-                                MazeTile::Wall => wall_color,
-                            };
-                            render_board.set(x, y, rgb);
-                        }
-                    }
-                    let exit_distance = distance(self.exit_pos.0, self.exit_pos.1);
-                    let exit_wall_intensity = (255.0 * (1.0 - exit_distance / 3.0)) as u8;
-                    let exit_wall_color = RGB::new(exit_wall_intensity, 0, 0);
-                    render_board.set(self.exit_pos.0, self.exit_pos.1, exit_wall_color);
-                    render_board.set(
-                        self.player_pos[0].0,
-                        self.player_pos[0].1,
-                        RGB::new(0, 255, 0),
-                    );
-                }
-                MazeGameMode::FlashLightMultiplayer => {
-                    let distance = |x: usize, y: usize, player_index: usize| {
-                        let dx = x as isize - self.player_pos[player_index].0 as isize;
-                        let dy = y as isize - self.player_pos[player_index].1 as isize;
-                        sqrt((dx * dx + dy * dy) as f64)
-                    };
-                    for x in 0..GRID_SIZE {
-                        for y in 0..GRID_SIZE {
-                            let mut max_wall_intensity = 0;
+                    MazeGameMode::FlashLight | MazeGameMode::FlashLightMultiplayer => {
+                        let distance = |x: usize, y: usize, player: &Player| {
+                            let dx = x as isize - player.position.0 as isize;
+                            let dy = y as isize - player.position.1 as isize;
+                            sqrt((dx * dx + dy * dy) as f64)
+                        };
 
-                            for player_index in 0..2 {
-                                let d = distance(x, y, player_index);
-                                let wall_intensity = (255.0 * (1.0 - d / 3.0)) as u8;
-
-                                if wall_intensity > max_wall_intensity {
-                                    max_wall_intensity = wall_intensity;
+                        for x in 0..GRID_SIZE {
+                            for y in 0..GRID_SIZE {
+                                let mut max_intensity = 0;
+                                for player in &self.players {
+                                    let d = distance(x, y, player);
+                                    let intensity = (255.0 * (1.0 - d / 3.0)).max(0.0) as u8;
+                                    max_intensity = max_intensity.max(intensity);
                                 }
-                            }
 
-                            let wall_color = RGB::new(
-                                max_wall_intensity,
-                                max_wall_intensity,
-                                max_wall_intensity,
-                            );
-                            let rgb = match self.board.tiles[x][y] {
-                                MazeTile::Empty => RGB::new(0, 0, 0),
-                                MazeTile::Wall => wall_color,
-                            };
-
-                            render_board.set(x, y, rgb);
-                        }
-                    }
-                    let min_exit_distance = |x: usize, y: usize| {
-                        let mut min_d = distance(x, y, 0);
-                        for player_index in 0..2 {
-                            let d = distance(x, y, player_index);
-                            if d < min_d {
-                                min_d = d;
+                                let rgb = match self.board.tiles[x][y] {
+                                    MazeTile::Empty => RGB::new(0, 0, 0),
+                                    MazeTile::Wall => {
+                                        RGB::new(max_intensity, max_intensity, max_intensity)
+                                    }
+                                };
+                                render_board.set(x, y, rgb);
                             }
                         }
-                        min_d
-                    };
-                    let exit_distance = min_exit_distance(self.exit_pos.0, self.exit_pos.1);
-                    let exit_wall_intensity = (255.0 * (1.0 - exit_distance / 3.0)) as u8;
-                    let exit_wall_color = RGB::new(exit_wall_intensity, 0, 0);
-                    render_board.set(self.exit_pos.0, self.exit_pos.1, exit_wall_color);
-                    render_board.set(
-                        self.player_pos[0].0,
-                        self.player_pos[0].1,
-                        RGB::new(0, 255, 0),
-                    );
-                    render_board.set(
-                        self.player_pos[1].0,
-                        self.player_pos[1].1,
-                        RGB::new(0, 0, 255),
-                    );
-                    if self.player_pos[0] == self.player_pos[1] {
+
+                        // Render exit
+                        let exit_intensity = self
+                            .players
+                            .iter()
+                            .map(|p| distance(self.exit_pos.0, self.exit_pos.1, p))
+                            .map(|d| (255.0 * (1.0 - d / 3.0)).max(0.0) as u8)
+                            .max()
+                            .unwrap_or(0);
                         render_board.set(
-                            self.player_pos[0].0,
-                            self.player_pos[0].1,
-                            RGB::new(0, 128, 128),
+                            self.exit_pos.0,
+                            self.exit_pos.1,
+                            RGB::new(exit_intensity, 0, 0),
                         );
+
+                        // Render players
+                        for player in &self.players {
+                            render_board.set(player.position.0, player.position.1, player.color);
+                        }
                     }
                 }
-            },
+            }
             MazeGameState::Victory(elapsed_time) => {
-                for row in 0..GRID_SIZE {
-                    for col in 0..GRID_SIZE {
-                        let color = self.generate_psychedelic_color(row, col, *elapsed_time);
-                        render_board.set(col, row, color);
+                if let Some(winner_index) = self.winning_player {
+                    let winner_color = self.players[winner_index].color;
+                    for row in 0..GRID_SIZE {
+                        for col in 0..GRID_SIZE {
+                            let color = self.generate_psychedelic_color(
+                                row,
+                                col,
+                                *elapsed_time,
+                                winner_color,
+                            );
+                            render_board.set(col, row, color);
+                        }
                     }
                 }
                 // Render players
-                render_board.set(
-                    self.player_pos[0].0,
-                    self.player_pos[0].1,
-                    RGB::new(0, 255, 0),
-                );
-                if let MazeGameMode::Multiplayer | MazeGameMode::FlashLightMultiplayer = self.mode {
-                    render_board.set(
-                        self.player_pos[1].0,
-                        self.player_pos[1].1,
-                        RGB::new(0, 0, 255),
-                    );
+                for player in &self.players {
+                    render_board.set(player.position.0, player.position.1, player.color);
                 }
             }
         }
